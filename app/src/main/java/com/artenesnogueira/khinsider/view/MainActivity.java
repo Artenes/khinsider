a026ae06dd07815771b4ef1b9667d8cc45cbe34f
Artenes Nogueira <artenesama@gmail.com>
2019-01-18 23:34:18 -0400

    Adds link to poen search activity

--- app/src/main/java/com/artenesnogueira/khinsider/view/MainActivity.java
+++ app/src/main/java/com/artenesnogueira/khinsider/view/MainActivity.java
@@ -1,8 +1,11 @@
 package com.artenesnogueira.khinsider.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.artenesnogueira.khinsider.R;
 import com.artenesnogueira.khinsider.adapter.MainActivityPageAdapter;
@@ -23,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MainActivityPageAdapter(getSupportFragmentManager());
         mViewPager = findViewById(R.id.pager);
         mViewPager.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.search_action) {
+            startActivity(new Intent(this, SearchActivity.class));
+        }
+        return true;
     }
 }
